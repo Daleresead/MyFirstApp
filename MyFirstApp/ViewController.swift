0db45f89d841e5e968eb510ab970b8fe1c86165d
Anatolij <vakulin.anatolij@bk.ru>
2024-10-05 14:06:27 +0300

    Первое приложение

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var StartButton: UIButton!
+    @IBOutlet weak var MainText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonTapped(_ sender: Any) {
+       
+        view.backgroundColor = .systemBackground
+        MainText.text = "Hi there"
+        MainText.textColor = .label
+        MainText.textAlignment = .center
+        
+        StartButton.setTitle("Получилось", for: .normal)
+    }
+    
 }
 
